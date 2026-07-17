aea682377d50bed48e005f3e1f28aebf01ca3e83
Oshin <oshinrathore3000@gmail.com>
2026-07-17 10:04:39 +0530

    Add solution for 282A Bit++

---
+++ 282A_Bit++.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int x = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i < n;i++){
+            String s = sc.next();
+            if(s.contains("++")){
+                x++;
+            }else{
+                x--;
+            }
+        }
+        System.out.println(x);
+    }
+}
