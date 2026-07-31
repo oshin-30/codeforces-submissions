42f6ea76bba0272ecc9bca5c2753554a9225ae90
Oshin <oshinrathore3000@gmail.com>
2026-07-31 20:19:23 +0530

    Add solution for 116A Tram

---
+++ 116A_Tram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = 0;
+        int sum = 0;
+        for(int i = 0;i < n;i++){   
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum = sum - a + b;
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
