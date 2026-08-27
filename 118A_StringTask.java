09e18b1242f55ab40180e2e5b6f465c6ab63a7b0
Oshin <oshinrathore3000@gmail.com>
2026-08-27 20:55:49 +0530

    Add solution for 118A String Task

---
+++ 118A_StringTask.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next().toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i < s.length();i++){
+            if(s.charAt(i) != 'a' && s.charAt(i) != 'e' && s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i) != 'u' && s.charAt(i) != 'y'){
+                sb.append('.');
+                sb.append(s.charAt(i));
+            }
+        }
+        System.out.println(sb);
+    }
+}
