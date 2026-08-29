f1f58fca76fe4d4413bf743c33058dd9dfeb23a3
Oshin <oshinrathore3000@gmail.com>
2026-08-29 21:12:06 +0530

    Add solution for 281A Word Capitalization

---
+++ 281A_WordCapitalization.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        s = Character.toUpperCase(s.charAt(0)) + s.substring(1);
+        System.out.println(s);
+    }
+}
