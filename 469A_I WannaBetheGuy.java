6a9e1bf6afba48656d159cec9a4021f5f230c57d
Oshin <oshinrathore3000@gmail.com>
2026-09-09 12:25:18 +0530

    Add solution for 469A I Wanna Be the Guy

---
+++ 469A_I WannaBetheGuy.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] levels = new boolean[n + 1];
+        int p = sc.nextInt();
+        for (int i = 0; i < p; i++) {
+            levels[sc.nextInt()] = true;
+        }
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            levels[sc.nextInt()] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!levels[i]) {
+                System.out.println("Oh, my keyboard!");
+                return;
+            }
+        }
+        System.out.println("I become the guy.");
+    }
+}
